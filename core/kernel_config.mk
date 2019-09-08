67aa59b73d39974f9e296a356c9f563d22075095
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-07 17:01:13 -0700

    kernel: Use absolute path for kernel O= argument
    The kernel make command is executed in the build root. Without absolute
    out paths, kernel out start with "out/" meaning it would be created in
    the kernel source. Add BUILD_ROOT to the argument to ensure it's always
    made in the actual out/ dir.
    
    Change-Id: I425134a893d4d5c9f31efccaf75e153b5462b76c

--- core/kernel_config.mk
+++ core/kernel_config.mk
@@ -141,3 +141,11 @@ KERNEL_MAKE_FLAGS += LEX=$(BUILD_TOP)/prebuilts/build-tools/$(HOST_PREBUILT_TAG)
 KERNEL_MAKE_FLAGS += YACC=$(BUILD_TOP)/prebuilts/build-tools/$(HOST_PREBUILT_TAG)/bin/bison
 KERNEL_MAKE_FLAGS += M4=$(BUILD_TOP)/prebuilts/build-tools/$(HOST_PREBUILT_TAG)/bin/m4
 TOOLS_PATH_OVERRIDE += BISON_PKGDATADIR=$(BUILD_TOP)/prebuilts/build-tools/common/bison
+
+# Set the out dir for the kernel's O= arg
+# This needs to be an absolute path, so only set this if the standard out dir isn't used
+OUT_DIR_PREFIX := $(shell echo $(OUT_DIR) | sed -e 's|/target/.*$$||g')
+KERNEL_BUILD_OUT_PREFIX :=
+ifeq ($(OUT_DIR_PREFIX),out)
+KERNEL_BUILD_OUT_PREFIX := $(BUILD_TOP)/
+endif
