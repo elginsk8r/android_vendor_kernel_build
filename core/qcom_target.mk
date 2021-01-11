7c853ed12247ea5865ba97e4b383eb9348a05521
Alessandro Astone <ales.astone@gmail.com>
2021-01-11 23:51:49 +0100

    build: Define QC_OPEN_PATH
    All references in sm7250 repos point to the correct location
    if we simply define QC_OPEN_PATH as vendor/qcom/opensource
    
    Change-Id: Ibb580230212a08c73a251c88630217b192979eb4

--- core/qcom_target.mk
+++ core/qcom_target.mk
@@ -10,6 +10,8 @@ $(if $(USE_DEVICE_SPECIFIC_$(1)), \
 $(call project-set-path,qcom-$(2),$(strip $(path)))
 endef
 
+QC_OPEN_PATH := vendor/qcom/opensource
+
 ifeq ($(BOARD_USES_QTI_HARDWARE),true)
 
 $(call set-device-specific-path,AUDIO,audio,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/audio)
@@ -17,8 +19,8 @@ $(call set-device-specific-path,DISPLAY,display,hardware/qcom-caf/$(QCOM_HARDWAR
 $(call set-device-specific-path,MEDIA,media,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/media)
 
 $(call set-device-specific-path,BT_VENDOR,bt-vendor,hardware/qcom-caf/bt)
-$(call set-device-specific-path,DATA_IPA_CFG_MGR,data-ipa-cfg-mgr,vendor/qcom/opensource/data-ipa-cfg-mgr)
-$(call set-device-specific-path,DATASERVICES,dataservices,vendor/qcom/opensource/commonsys/dataservices)
+$(call set-device-specific-path,DATA_IPA_CFG_MGR,data-ipa-cfg-mgr,$(QC_OPEN_PATH)/data-ipa-cfg-mgr)
+$(call set-device-specific-path,DATASERVICES,dataservices,$(QC_OPEN_PATH)/commonsys/dataservices)
 $(call set-device-specific-path,VR,vr,hardware/qcom-caf/vr)
 $(call set-device-specific-path,WLAN,wlan,hardware/qcom-caf/wlan)
 
@@ -37,15 +39,15 @@ endif
 
 ifeq ($(TARGET_USE_QTI_BT_STACK),true)
 PRODUCT_SOONG_NAMESPACES += \
-    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
-    vendor/qcom/opensource/commonsys/system/bt/conf
+    $(QC_OPEN_PATH)/commonsys/packages/apps/Bluetooth \
+    $(QC_OPEN_PATH)/commonsys/system/bt/conf
 endif #TARGET_USE_QTI_BT_STACK
 
 # Add display-commonsys-intf to PRODUCT_SOONG_NAMESPACES for QSSI supported platforms
 ifneq ($(filter $(QSSI_SUPPORTED_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
 PRODUCT_SOONG_NAMESPACES += \
-    vendor/qcom/opensource/commonsys-intf/display \
-    vendor/qcom/opensource/commonsys/display
+    $(QC_OPEN_PATH)/commonsys-intf/display \
+    $(QC_OPEN_PATH)/commonsys/display
 endif
 
 PRODUCT_CFI_INCLUDE_PATHS += \
