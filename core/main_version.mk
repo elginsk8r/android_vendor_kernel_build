32b691bc36cc0b4ceeef83ce207bf2cb592411b8
Nick Reuter <nreuter85@gmail.com>
2021-10-05 23:05:45 -0500

    build: ADDITIONAL_BUILD_PROPERTIES -> ADDITIONAL_SYSTEM_PROPERTIES
    Change-Id: Ia312839f726ceffc9ac08aab138e10debe6f443c

--- core/main_version.mk
+++ core/main_version.mk
@@ -1,19 +1,19 @@
 # Build fingerprint
 ifneq ($(BUILD_FINGERPRINT),)
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 endif
 
 ifeq ($(PRODUCT_OVERRIDE_INFO),true)
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.build.stock_fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT)
 endif
 
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.evervolv.display.version=$(EV_VERSION) \
     ro.evervolv.device=$(EV_CODENAME) \
     ro.evervolv.version=$(TARGET_OTA_PACKAGE_NAME)
 
-ADDITIONAL_BUILD_PROPERTIES += \
+ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.evervolv.build.version.plat.sdk=$(EV_PLATFORM_SDK_VERSION) \
     ro.evervolv.build.version.plat.rev=$(EV_PLATFORM_REV)
