413010bbc8ae5caf5e1adc30d504e41a8714027b
Nolen Johnson <johnsonnolen@gmail.com>
2022-02-14 15:57:50 -0500

    products: lineage_x86{,_64}: Don't include SDK addon
    * This doesn't work, and just errors out similar to arm/arm64.
    
    Change-Id: I53a2261095f1e2acab3b7da20d3f092ccc83e8ab

--- target/product/ev_x86_64.mk
+++ target/product/ev_x86_64.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2018-2020 The LineageOS Project
+# Copyright (C) 2018-2022 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,6 +21,3 @@ include $(SRC_EVERVOLV_DIR)/build/target/product/evervolv.mk
 PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := relaxed
 
 PRODUCT_NAME := ev_x86_64
-
-PRODUCT_SDK_ADDON_NAME := evervolv
-PRODUCT_SDK_ADDON_SYS_IMG_SOURCE_PROP := $(LOCAL_PATH)/source.properties
