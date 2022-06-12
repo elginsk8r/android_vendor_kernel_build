aa8432a664c8471312d91cfc1480cc0cbfda5916
Nick Reuter <nreuter85@gmail.com>
2022-06-12 00:20:04 -0500

    Revert "build: remove stock fingerprint override"
    This reverts commit eec8022da86ead10f328bb8fc8a894e5adfe967d.
    
    Change-Id: Iace661a37780d928b3d8dd23e7ab6b25a6595ce1

--- core/main_version.mk
+++ core/main_version.mk
@@ -4,6 +4,17 @@ ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 endif
 
+# Override fingerprint for Google Play Services and SafetyNet
+ifneq (,$(findstring $(BUILD_ID),$(BUILD_FINGERPRINT)))
+PRODUCT_OVERRIDE_FINGERPRINT := $(BUILD_FINGERPRINT)
+endif
+PRODUCT_OVERRIDE_FINGERPRINT ?= google/redfin/redfin:12/SQ3A.220605.009.A1/8643238:user/release-keys
+
+ifneq ($(PRODUCT_OVERRIDE_FINGERPRINT),)
+ADDITIONAL_SYSTEM_PROPERTIES += \
+    ro.build.stock_fingerprint=$(PRODUCT_OVERRIDE_FINGERPRINT)
+endif
+
 ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.evervolv.device=$(TARGET_DEVICE) \
     ro.evervolv.releasetype=$(EV_BUILD_TYPE) \
